d6c99c879b9b4ea222626786518a8cd91f0e450a
lagodukn <thiagolagaresgon@gmail.com>
2024-07-31 17:24:25 -0300

    some excludes

--- exec/test.py
+++ exec/test.py
@@ -1,4 +1,3 @@
-# COLETA E VERIFICAÇÃO DOS INPUTS NECESSÁRIOS PARA A ENTRADA NO SITE
 campaign_id = [
     {"campaign_name":"SECOM_P", "url":"https://ads.kwai.com/?accountId=60161580#/index", "ativos": " "},
     {"campaign_name":"teste", "url":"https://ads.kwai.com/?accountId=60161580#/management", "ativos" : " "},
@@ -12,7 +11,6 @@ for i in range(len(campaign_id)):
         print(f'A campanha {campaign_id[i]['campaign_name']} já possui {campaign_id[i]['ativos']} ativos adicionados a base do robô.')
 print(f'{campaign_id}')
 
-# ENTRARf NO SITE
 import pyautogui as pg
 import time 
 
@@ -41,7 +39,6 @@ pg.write(f'{campaign_id[0]['campaign_name']}', interval=0.10)
 pg.click(x=1234, y=489, clicks=1)
 time.sleep(7)
 
-# ETAPAS DENTRO DO SITE
 while i < 3:
     print(f'{i}')
     pg.press("tab")
@@ -49,6 +46,7 @@ while i < 3:
     i += 1    
 i = 0
 pg.press("enter")
+
 # COLETA DO DADO
 # CONFIRMAR VERACIDADE DO DADO
 
--- input/input.py
+++ input/input.py
@@ -1,35 +1,27 @@
 import json
 import os
 
-# Verifica se o arquivo config.json existe
 if os.path.exists('config.json'):
-    # Carrega o conteúdo do arquivo
     with open('config.json', 'r') as f:
         campaign_id = json.load(f)
 else:
-    # Se o arquivo não existe, cria um novo com o conteúdo padrão
     campaign_id = [
         {"campaign_name": "SECOM_P", "url": "https://ads.kwai.com/?accountId=60161580#/index", "ativos": " ", "startdate":" ", "enddate":" "},
         {"campaign_name": "teste", "url": "https://ads.kwai.com/?accountId=60161580#/management", "ativos": " ", "startdate":" ", "enddate":" "},
     ]
 
-# Verifica se o conteúdo do arquivo é válido
 if not isinstance(campaign_id, list):
     print("Erro: o conteúdo do arquivo config.json é inválido.")
     exit(1)
 
-# Loop pelas campanhas
 for i in range(len(campaign_id)):
-    # Verifica se o número de ativos já foi definido
     if campaign_id[i]['ativos'] == ' ':
         print(f'A campanha {campaign_id[i]["campaign_name"]} ainda não tem o número de ativos selecionado.')
         campaign_id[i]['ativos'] = input('Digite o número de ativos desta campanha: ')
     else:
         print(f'A campanha {campaign_id[i]["campaign_name"]} já possui {campaign_id[i]["ativos"]} ativos adicionados a base do robô.')
-# Pergunta se deseja adicionar uma nova campanha
 add_campaign = input('Deseja adicionar uma nova campanha? (y/n): ')
 if add_campaign.lower() == 'y':
-    # Adiciona uma nova campanha
     new_campaign_name = input('Digite a palavra-chave da nova campanha: ')
     new_campaign_url = input('Digite a URL da nova campanha somente até #/: ')
     new_campaign_ativos = input('Digite o número de ativos da nova campanha: ')
@@ -40,15 +32,12 @@ if add_campaign.lower() == 'y':
     })
     print(f'Seus novos dados são:\n{campaign_id}')
 
-# Verifica se o conteúdo do arquivo mudou
 if os.path.exists('config.json'):
     with open('config.json', 'r') as f:
         old_campaign_id = json.load(f)
     if old_campaign_id != campaign_id:
-        # Se o conteúdo mudou, salva o novo conteúdo
         with open('config.json', 'w') as f:
             json.dump(campaign_id, f)
 else:
-    # Se o arquivo não existe, salva o conteúdo padrão
     with open('config.json', 'w') as f:
         json.dump(campaign_id, f)
