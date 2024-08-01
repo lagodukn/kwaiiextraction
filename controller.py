5bc3079e0fd458ae0cac9fb89eeb7af9fc62930d
lagodukn <thiagolagaresgon@gmail.com>
2024-08-01 18:18:26 -0300

    selenium updates

--- controller.py
+++ controller.py
@@ -1,1 +1,28 @@
-# este arquivo irá chamar o input, inside e o output. 
+# este arquivo irá chamar o input, inside e o output
+import time
+import os
+import json
+import pyautogui as pg
+from datetime import date
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+if os.path.exists('config.json'):
+    with open('config.json', 'r') as f:
+        data = json.load(f)
+else:
+    print('Não foi possível encontrar os dados de Configuração.')
+
+chrome_options = Options() 
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.maximize_window()
+driver.get(f'https://ads.kwai.com/?accountId={data[0]['id']}#/report/customReport')
+time.sleep(20)
+
+driver.find_element(By.ID, "email").send_keys("performance@novasb.com.br" )
+driver.find_element(By.ID, "password").send_keys("$NOVASB$2022@")
+driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div[2]/div[2]/div[2]/button").click()
+time.sleep(30)
--- exec/test.py
+++ exec/test.py
@@ -4,14 +4,14 @@ import os
 import json
 from datetime import date
 
+# trazendo os dados
 if os.path.exists('config.json'):
     with open('config.json', 'r') as f:
         data = json.load(f)
 else:
     print('Não foi possível encontrar os dados de Configuração.')
 
-print(data)
-
+# entrando no Chrome no perfil selecionado
 pg.press("win")
 time.sleep(1)
 pg.write("Chrome", interval=0.10)
@@ -22,13 +22,14 @@ pg.press("tab")
 time.sleep(1)
 pg.press("enter")
 time.sleep(1)
+
+# entrando no site em questão
 pg.hotkey('ctrl', 't')
 pg.write(f'{data[0]['url']}', interval=0.10)
 pg.press("enter")
 time.sleep(30)
 i = 0
 while i < 7:
-    print(f'{i}')
     pg.press("tab")
     time.sleep(0.5)
     i += 1
@@ -37,35 +38,75 @@ pg.write(f'{data[0]['id']}', interval=0.10)
 pg.click(x=1234, y=489, clicks=1)
 time.sleep(7)
 
-while i < 3:
-    print(f'{i}')
-    pg.press("tab")
-    time.sleep(0.5)
-    i += 1    
-i = 0
-pg.press("enter")
+# entrada no link de destino final de coleta
+pg.hotkey('ctrl', 'l')
+pg.press('right')
 time.sleep(2)
-# # input da data
+pg.keyDown('shiftleft')
+pg.keyDown('shiftright')
+pg.keyDown('ctrl')
+pg.press('left')
+pg.keyUp('shiftleft')
+pg.keyUp('shiftright')
+pg.keyUp('ctrl')
+pg.press('delete')
+time.sleep(2)
+pg.write('report/customReport', interval=0.10)
+pg.press('enter')
+i = 0
 
-
-while i < (14 + int(data[0]['ativos'])):
+while i < 15:
     pg.press("tab")
     time.sleep(0.5)
-    if i == 7:
+    if i == 14:
         pg.write(data[0]['startdate'])
-        pg.press('enter')
-        pg.write(f'{date.today()}')
-        pg.press('enter')
         time.sleep(3)
-        i += 1
-    elif i in [13 + 2*j for j in range(int(data[0]['ativos']))]:
-        print(f'{i}')
-        pg.press('space')
-        time.sleep(2)
+        pg.press('tab')
+        time.sleep(0.5)
+        pg.press('tab')
+        pg.write(f'{date.today()}')
+        time.sleep(3)
+        pg.press('enter')
         i += 1
     else:
         i +=1
 i = 0
+
+while i < 5:
+    pg.press("tab")
+    time.sleep(0.5)
+    if i == 4:
+        pg.press('enter')
+        time.sleep(2)
+        x, y = pg.position()
+        pg.click(x, y)
+        i += 1
+    else:
+        i += 1
+i = 0
+# inicio do processo de coleta
+
+
+
+# # entrada nos checks de ativos
+# while i < (14 + int(data[0]['ativos'])):
+#     pg.press("tab")
+#     time.sleep(0.5)
+#     if i == 7:
+#         pg.write(data[0]['startdate'])
+#         pg.press('enter')
+#         pg.write(f'{date.today()}')
+#         pg.press('enter')
+#         time.sleep(3)
+#         i += 1
+#     elif i in [13 + 2*j for j in range(int(data[0]['ativos']))]:
+#         print(f'{i}')
+#         pg.press('space')
+#         time.sleep(2)
+#         i += 1
+#     else:
+#         i +=1
+# i = 0
 
 # # CONFIRMAR VERACIDADE DO DADO
 
